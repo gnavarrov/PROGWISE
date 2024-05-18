279a1f8fed411d61a6a72469c8980042a47943f8
valentina <valenalibrandi@gmail.com>
2024-05-18 12:02:21 +0200

    Login

--- frontend/src/app/form-login/form-login.component.ts
+++ frontend/src/app/form-login/form-login.component.ts
@@ -6,7 +6,7 @@ import { AuthService } from '../services/auth.service';
 @Component({
   selector: 'app-form-login',
   templateUrl: './form-login.component.html',
-  styleUrls: ['./form-login.component.scss']  // Añadir este estilo
+  styleUrls: ['./form-login.component.scss']  // Añadir este estilo si existe el archivo
 })
 export class FormLoginComponent implements OnInit {
   loginForm!: FormGroup;
@@ -28,22 +28,31 @@ export class FormLoginComponent implements OnInit {
   onLogin(): void {
     if (this.loginForm.valid) {
       const formData = {
-        email: this.loginForm.value.email,
-        password: this.loginForm.value.password
+        correo: this.loginForm.value.email,
+        contraseña: this.loginForm.value.password
       };
+
 
       this.authService.loginUser(formData).subscribe({
         next: (response) => {
-          console.log('Inicio de sesión exitoso', response);
-          this.router.navigate(['/dashboard']);  // Redirige al usuario al dashboard
+          console.log('Respuesta del servidor:', response);
+          if (response && response.message === 'Inicio de sesión exitoso') {
+            console.log('Inicio de sesión exitoso');
+            this.router.navigate(['/dashboard']); // Redirigir al usuario al dashboard
+          } else {
+            this.errorMessage = 'Credenciales incorrectas. Por favor, inténtalo de nuevo.';
+            this.loginForm.reset(); // Limpiar el formulario
+            // También podrías mostrar una alerta aquí si lo prefieres
+          }
         },
         error: (error) => {
-          console.error('Error al iniciar sesión', error);
-          this.errorMessage = 'Credenciales incorrectas. Por favor, inténtalo de nuevo.';
+          console.error('Error al iniciar sesión:', error);
+          this.errorMessage = 'Error al iniciar sesión. Por favor, inténtalo de nuevo más tarde.';
           this.loginForm.reset(); // Limpiar el formulario
           // También podrías mostrar una alerta aquí si lo prefieres
         }
       });
-    }
+
+}
   }
 }
